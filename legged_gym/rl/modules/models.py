648cc95e355d750ce5e75c99dca001adc7af747a
alescontrela <escontrela@berkeley.edu>
2025-04-09 12:38:41 -0700

    undo Memory delete

--- legged_gym/rl/modules/models.py
+++ legged_gym/rl/modules/models.py
@@ -185,3 +185,32 @@ class RecurrentModel(torch.nn.Module):
       if hasattr(layer, 'stats'):
         stats.update(layer.stats())
     return stats
+
+
+class Memory(torch.nn.Module):
+    def __init__(self, input_size, type='lstm', num_layers=1, hidden_size=256):
+        super().__init__()
+        # RNN
+        rnn_cls = nn.GRU if type.lower() == 'gru' else nn.LSTM
+        self.rnn = rnn_cls(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers)
+        self.hidden_states = None
+    
+    def forward(self, input, masks=None, hidden_states=None, upd_state=True):
+        batch_mode = masks is not None
+        if batch_mode:
+            # batch mode (policy update): need saved hidden states
+            if hidden_states is None:
+                raise ValueError("Hidden states not passed to memory module during policy update")
+            out, _ = self.rnn(input, hidden_states)
+            out = unpad_trajectories(out, masks)
+        else:
+            # inference mode (collection): use hidden states of last step
+            out, new_hidden_states = self.rnn(input.unsqueeze(0), self.hidden_states)
+            if upd_state:
+                self.hidden_states = new_hidden_states
+        return out
+
+    def reset(self, dones=None):
+        # When the RNN is an LSTM, self.hidden_states_a is a list with hidden_state and cell_state
+        for hidden_state in self.hidden_states:
+            hidden_state[..., dones, :] = 0.0
