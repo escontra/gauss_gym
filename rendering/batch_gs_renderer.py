68737af90b74b5416946d39689f061463a2dafcb
alescontrela <escontrela@berkeley.edu>
2025-06-16 18:07:27 -0700

    add motion blur support

--- rendering/batch_gs_renderer.py
+++ rendering/batch_gs_renderer.py
@@ -7,6 +7,68 @@ from gsplat.rendering import rasterization
 from torch import Tensor
 from jaxtyping import Float
 import json
+from einops import rearrange, repeat
+
+from legged_gym import utils
+
+
+def exp_map_SE3(tangent_vector: Float[Tensor, "b 6"]) -> Float[Tensor, "b 3 4"]:
+    """Compute the exponential map `se(3) -> SE(3)`.
+
+    This can be used for learning pose deltas on `SE(3)`.
+
+    Args:
+        tangent_vector: A tangent vector from `se(3)`.
+
+    Returns:
+        [R|t] transformation matrices.
+    """
+
+    tangent_vector_lin = tangent_vector[:, :3].view(-1, 3, 1)
+    tangent_vector_ang = tangent_vector[:, 3:].view(-1, 3, 1)
+
+    theta = torch.linalg.norm(tangent_vector_ang, dim=1).unsqueeze(1)
+    theta2 = theta**2
+    theta3 = theta**3
+
+    near_zero = theta < 1e-2
+    non_zero = torch.ones(1, dtype=tangent_vector.dtype, device=tangent_vector.device)
+    theta_nz = torch.where(near_zero, non_zero, theta)
+    theta2_nz = torch.where(near_zero, non_zero, theta2)
+    theta3_nz = torch.where(near_zero, non_zero, theta3)
+
+    # Compute the rotation
+    sine = theta.sin()
+    cosine = torch.where(near_zero, 8 / (4 + theta2) - 1, theta.cos())
+    sine_by_theta = torch.where(near_zero, 0.5 * cosine + 0.5, sine / theta_nz)
+    one_minus_cosine_by_theta2 = torch.where(near_zero, 0.5 * sine_by_theta, (1 - cosine) / theta2_nz)
+    ret = torch.zeros(tangent_vector.shape[0], 3, 4).to(dtype=tangent_vector.dtype, device=tangent_vector.device)
+    ret[:, :3, :3] = one_minus_cosine_by_theta2 * tangent_vector_ang @ tangent_vector_ang.transpose(1, 2)
+
+    ret[:, 0, 0] += cosine.view(-1)
+    ret[:, 1, 1] += cosine.view(-1)
+    ret[:, 2, 2] += cosine.view(-1)
+    temp = sine_by_theta.view(-1, 1) * tangent_vector_ang.view(-1, 3)
+    ret[:, 0, 1] -= temp[:, 2]
+    ret[:, 1, 0] += temp[:, 2]
+    ret[:, 0, 2] += temp[:, 1]
+    ret[:, 2, 0] -= temp[:, 1]
+    ret[:, 1, 2] -= temp[:, 0]
+    ret[:, 2, 1] += temp[:, 0]
+
+    # Compute the translation
+    sine_by_theta = torch.where(near_zero, 1 - theta2 / 6, sine_by_theta)
+    one_minus_cosine_by_theta2 = torch.where(near_zero, 0.5 - theta2 / 24, one_minus_cosine_by_theta2)
+    theta_minus_sine_by_theta3_t = torch.where(near_zero, 1.0 / 6 - theta2 / 120, (theta - sine) / theta3_nz)
+
+    ret[:, :, 3:] = sine_by_theta * tangent_vector_lin
+    ret[:, :, 3:] += one_minus_cosine_by_theta2 * torch.cross(tangent_vector_ang, tangent_vector_lin, dim=1)
+    ret[:, :, 3:] += theta_minus_sine_by_theta3_t * (
+        tangent_vector_ang @ (tangent_vector_ang.transpose(1, 2) @ tangent_vector_lin)
+    )
+    ret = torch.cat([ret, torch.zeros(ret.shape[0], 1, 4, device=ret.device)], dim=1)
+    ret[:, 3, 3] = 1.0 # homogeneous
+    return ret
 
 
 class BatchPLYRenderer:
@@ -87,6 +149,8 @@ class BatchPLYRenderer:
         camera_linear_velocity: Union[Float[Tensor, "num_cameras 3"], None] = None,
         camera_angular_velocity: Union[Float[Tensor, "num_cameras 3"], None] = None,
         motion_blur_frac: float = 0.0,
+        blur_samples: int = 5,
+        blur_dt: float = 0.03,
         minibatch: int = 15,
         out_device: str = 'cpu',
     ) -> Tuple[Tensor, Tensor]:
@@ -131,6 +195,30 @@ class BatchPLYRenderer:
         # Render in batches
         for i in range(0, num_images, minibatch):
             batch_w2cs = w2cs[i:i + minibatch]
+
+            original_B_size = len(batch_w2cs)
+            # for simplicity either blur the whole minibatch or none. in expectation this is the same as 
+            # a ratio of motion blur fraction
+            blur_batch = torch.rand(1).item() < motion_blur_frac
+            if blur_batch:
+                # compute `blur_samples` more cameras along the velocity vectors to render from
+                # negative dt because we want to move backwards in time to render blur from the past
+                dts = -torch.linspace(0, blur_dt, blur_samples, device=self.device)
+                # interleave the original cameras to render more
+                batch_w2cs = repeat(batch_w2cs, 'batch mat1 mat2 -> (batch r) mat1 mat2', r = blur_samples)
+                # grab the 6-dof velocity vectors
+                batch_vels = torch.cat([camera_linear_velocity[i:i + minibatch], camera_angular_velocity[i:i + minibatch]], dim=-1) # B, 6
+                # also repeat the velocity vectors for each of the blur samples
+                batch_vels = repeat(batch_vels, 'batch vel -> (batch r) vel', r = blur_samples)
+                # repeat the dt for each of the blur samples
+                dts = repeat(dts, 'samples -> (batch samples) 1', batch = original_B_size)
+                # compute the delta matrices
+                batch_delta_mats = exp_map_SE3(batch_vels * dts)# (B r) 4 4
+                # apply the delta matrices to the original cameras in world frame
+                # this is equivalent to DT @ C2w, BUT gsplat takes in a w2c matrix so we need to do (DT @ C2w)^-1 = (C2w)^-1 @ DT^-1
+                batch_w2cs = torch.bmm(batch_w2cs,torch.inverse(batch_delta_mats))
+            # If this OOMs, decrease minibatch size (blur uses 5x more memory)
+
             # Render using gsplat
             with torch.no_grad():
               colors, _, _ = rasterization(
@@ -148,6 +236,11 @@ class BatchPLYRenderer:
                   render_mode="RGB",
               )
             colors.clamp_(0, 1)  # Clamp colors to [0, 1]
+            if blur_batch:
+                # if we blurred then re-average the colors along the blur dimension
+                colors = rearrange(colors, '(b r) h w c -> b r h w c', r = blur_samples)
+                colors = colors.mean(dim=1)
+            assert colors.shape == (original_B_size, h, w, 3), f"Colors shape is {colors.shape} but should be {original_B_size, h, w, 3}"
             
             imgs_out[i:i + minibatch] = (255 * colors[..., :3].to(out_device, non_blocking=True)).to(torch.uint8)
             # depth_out[i:i + minibatch] = (depths / self.dataparser_scale).to(out_device, non_blocking=True)
@@ -168,12 +261,12 @@ class MultiSceneRenderer:
         self.renderer_gpus = [f"cuda:{gpu}" if isinstance(gpu, int) else gpu for gpu in renderer_gpus]
         self.output_gpu = f"cuda:{output_gpu}" if isinstance(output_gpu, int) else output_gpu
         self.renderers = {}
-        print(f'Rendering on GPUs: {self.renderer_gpus}, Outputting to GPU: {self.output_gpu}')
+        utils.print(f'Rendering on GPUs: {self.renderer_gpus}, Outputting to GPU: {self.output_gpu}', color='cyan')
         
         # Instantiate renderers for each PLY file, distributing across GPUs
         for i, ply_path in enumerate(ply_paths):
             gpu_device = self.renderer_gpus[i % len(self.renderer_gpus)]
-            print(f"Initializing renderer for {ply_path} on {gpu_device}")
+            utils.print(f"Initializing renderer for {ply_path} on {gpu_device}", color='cyan')
             self.renderers[ply_path] = BatchPLYRenderer(Path(ply_path), device=gpu_device)
     
     @torch.no_grad()
@@ -189,6 +282,8 @@ class MultiSceneRenderer:
         camera_linear_velocity: Union[Dict[str, Float[Tensor, "num_cameras 3"]], None] = None,
         camera_angular_velocity: Union[Dict[str, Float[Tensor, "num_cameras 3"]], None] = None,
         motion_blur_frac: float = 0.0,
+        blur_samples: int = 5,
+        blur_dt: float = 0.03,
         minibatch: int = 15
     ) -> Dict[str, Tuple[Float[Tensor, "num_cameras h w 3"], Float[Tensor, "num_cameras h w 1"]]]:
         """
@@ -226,6 +321,8 @@ class MultiSceneRenderer:
                   c2ws, fl_x, fl_y, pp_x, pp_y, h, w,
                   camera_linear_velocity=cam_lin_vel,
                   camera_angular_velocity=cam_ang_vel,
+                  motion_blur_frac=motion_blur_frac,
+                  blur_samples=blur_samples, blur_dt=blur_dt,
                   minibatch=minibatch, out_device=self.output_gpu)
               
               # Store results
