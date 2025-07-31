82594e64f0e3e685eaf0cf8b2e3bab49d313b2a8
alescontrela <escontrela@berkeley.edu>
2025-07-31 13:39:38 -0700

    revert to old rewards, noise ranges. Add Go1

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -9,5 +9,6 @@ from legged_gym.utils import config
 from legged_gym.utils.task_registry import task_registry
 
 task_registry.register("a1", LeggedRobot, config.from_yaml(pathlib.Path(legged_gym.GAUSS_GYM_ENVS_DIR) / "a1" / "config.yaml"))
+task_registry.register("go1", LeggedRobot, config.from_yaml(pathlib.Path(legged_gym.GAUSS_GYM_ENVS_DIR) / "go1" / "config.yaml"))
 task_registry.register("t1", T1, config.from_yaml(pathlib.Path(legged_gym.GAUSS_GYM_ENVS_DIR) / "t1" / "config.yaml"))
 task_registry.register("anymal_c", Anymal, config.from_yaml(pathlib.Path(legged_gym.GAUSS_GYM_ENVS_DIR) / "anymal_c" / "config.yaml"))
